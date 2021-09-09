3ee05771cf9ac13fa309baa94a023fc5243b0bd8
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 22:13:31 +0530

    Find Largest Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the alphabet : ");
-        char a = sc.next().charAt(0);
-        if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O'
-                || a == 'U') {
-            System.out.println("The " + a + " is Vowel");
-
-        } else if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
-            System.out.println("The " + a + " is Consonant");
-        else
-            System.out.println("Not an alphabet");
+        System.out.println("Enter the 1 no : ");
+        int a = sc.nextInt();
+        System.out.println("Enter the 2 no : ");
+        int b = sc.nextInt();
+        System.out.println("Enter the 3 no : ");
+        int c = sc.nextInt();
+        if (a > b && a > c) {
+            System.out.println("The no : " + a + " Largest no");
+        } else if (b > a && b > c) {
+            System.out.println("The no : " + b + " Largest no");
+        } else {
+            System.out.println("The no : " + c + " Largest no");
+        }
     }
 }
