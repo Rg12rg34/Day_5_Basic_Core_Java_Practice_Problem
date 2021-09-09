94f8b0eac136425497890ad22e2b9d3f523d4701
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 22:06:49 +0530

    Vowel or Consonant

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the 1 no : ");
-        int a = sc.nextInt();
-        if (a % 2 == 1)
-            System.out.println("Enter no is odd.");
+        System.out.println("Enter the alphabet : ");
+        char a = sc.next().charAt(0);
+        if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O'
+                || a == 'U') {
+            System.out.println("The " + a + " is Vowel");
+
+        } else if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+            System.out.println("The " + a + " is Consonant");
         else
-            System.out.println("Enter no is even.");
+            System.out.println("Not an alphabet");
     }
 }
