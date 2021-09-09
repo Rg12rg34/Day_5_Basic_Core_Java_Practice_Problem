424a31bb9071d417c7427c501191ef644b7a6c72
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 20:51:07 +0530

    Power of 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the year : ");
-        int year = sc.nextInt();
 
-        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
-            System.out.println("year is leap year");
-        } else {
-            System.out.println("year is not leap year");
+        int powerof2 = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the No : ");
+        int no = sc.nextInt();
+        for (int p = 0; p <= no; p++) {
+            System.out.println("No is : " + p + " Power of 2 is : " + powerof2);
+            powerof2 = 2 * powerof2;
         }
     }
 }
