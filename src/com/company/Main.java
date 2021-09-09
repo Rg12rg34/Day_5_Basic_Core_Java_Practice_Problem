059b912c4d1222691bc8ce62addec71a892ecd0d
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 20:30:50 +0530

    flip the coin

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int headwin = 0;
+        int tailwin = 0;
+        Random value = new Random();
+        for (int p = 0; p < 15; p++) {
+
+            int flip = value.nextInt(2);
+            if (flip < 0.5) {
+                tailwin++;
+
+            } else {
+                headwin++;
+
+            }
+        }
+        System.out.println("Tail" + tailwin);
+        System.out.println("Head" + headwin);
+    }
+
+}
