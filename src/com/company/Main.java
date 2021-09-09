284b400ddbaf104d1c82ba36233e2b8fda3fac8b
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 22:52:57 +0530

    Quadratic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter point A & B : ");
+        System.out.println("Enter point A & B & C : ");
         double a = sc.nextDouble();
         double b = sc.nextDouble();
+        double c = sc.nextDouble();
 
-        double DistCal = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-        System.out.println("The Distance is :  " + DistCal);
+        double delta = b * b - 4 * a * c;
+        double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+        System.out.println("Root 1 :" + root1);
+        System.out.println("Root 2 :" + root2);
     }
 }
