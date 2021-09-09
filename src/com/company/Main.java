9e6d78fd30bb6fd247c3e5f7b54ffae5f0215abd
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 21:44:43 +0530

    Evan odd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the 1 no : ");
         int a = sc.nextInt();
-        System.out.println("Enter the 2 no : ");
-        int b = sc.nextInt();
-        int c;
-        System.out.println("B4 swapping : " + a + " & " + b);
-        c = a;
-        a = b;
-        b = c;
-        System.out.println("After swapping : " + a + " & " + b);
+        if (a % 2 == 1)
+            System.out.println("Enter no is odd.");
+        else
+            System.out.println("Enter no is even.");
     }
 }
