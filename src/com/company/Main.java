199ca352aae2e2c912c1e267a68e4e2919de1382
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 21:11:24 +0530

    Quotient and Remainder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number ::");
-        int no = sc.nextInt();
+        System.out.println("Enter the dividend : ");
+        int dividend = sc.nextInt();
+        System.out.println("Enter the divisor : ");
+        int divisor = sc.nextInt();
 
-        for (int p = 2; p < no; p++) {
-            while (no % p == 0) {
-                System.out.println(p + " ");
-                no = no / p;
-            }
-        }
-        if (no > 2) {
-            System.out.println(no);
-        }
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
     }
 }
