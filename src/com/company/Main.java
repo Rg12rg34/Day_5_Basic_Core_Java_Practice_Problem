f01bf689634d77448a7551e654238c285323d135
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-09 21:29:30 +0530

    Swap Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the dividend : ");
-        int dividend = sc.nextInt();
-        System.out.println("Enter the divisor : ");
-        int divisor = sc.nextInt();
-
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
-
-        System.out.println("The Quotient is = " + quotient);
-        System.out.println("The Remainder is = " + remainder);
+        System.out.println("Enter the 1 no : ");
+        int a = sc.nextInt();
+        System.out.println("Enter the 2 no : ");
+        int b = sc.nextInt();
+        int c;
+        System.out.println("B4 swapping : " + a + " & " + b);
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("After swapping : " + a + " & " + b);
     }
 }
